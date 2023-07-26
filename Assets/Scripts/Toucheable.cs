9d29599c3520494d4da23727d863833d014f0e08
Diego Guzmán <diego.e.guz90@gmail.com>
2023-07-26 09:52:22 -0500

    Game scene with essentials

---
+++ Assets/Scripts/Toucheable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toucheable : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        Debug.Log("Drag!");
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -29,6 +30,7 @@ public class UIManager : MonoBehaviour
     void onClickStartBtn()
     {
         Debug.Log("Start button clicked!");
+        SceneManager.LoadScene(1);
     }
 
     void onClickExitBtn()
